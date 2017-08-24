7d45907d11d80519a0f5c1b7284306868da6a078
Lizzie Hard <elizabeth_hard@hotmail.co.uk>
2017-08-24 14:58:51 +0100

    added extra files

--- Gemfile
+++ Gemfile
@@ -4,7 +4,7 @@ ruby '2.4.0'
 gem 'data_mapper'
 gem 'dm-postgres-adapter'
 gem 'database_cleaner'
-
+gem 'pg'
 gem 'sinatra'
 
 group :test do
--- spec/creating_links_spec.rb
+++ spec/creating_links_spec.rb
@@ -1,23 +1,5 @@
 require 'database_cleaner'
 
-
-RSpec.configure do |config|
-  config.before(:suite) do
-    DatabaseCleaner.strategy = :transaction
-    DatabaseCleaner.clean_with(:truncation)
-  end
-
-  config.before(:each) do
-    DatabaseCleaner.start
-  end
-
-
-  config.after(:each) do
-    DatabaseCleaner.clean
-  end
-
-end
-
 feature 'adding links' do
 
   scenario 'link gets added to list' do
